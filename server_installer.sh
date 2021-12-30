a5071cde326856f4073628573d1030ee626904e5
RoderikTheRed <66172264+RoderikTheRed@users.noreply.github.com>
2021-12-30 17:22:47 +0100

    Add files via upload

---
+++ server_installer.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+RED="\e[31m"
+GREEN="\e[92m"
+DEFAULT="\e[35m"
+ENDCOLOR="\e[0m"
+
+echo -e "${DEFAULT}Apache2 & MySQL(PhpMyAdmin) Installer by RoderikTheRed${ENDCOLOR}"
+echo -e "${DEFAULT}INFO: Checking Privileges...${ENDCOLOR}"
+if [ "$EUID" -ne 0 ]
+	then echo -e "${RED}FAILED: This script requires Sudo/Root Privileges to run properly!${ENDCOLOR}"
+	exit
+fi
+echo -e "${DEFAULT}INFO: Updating System...${ENDCOLOR}"
+apt -qq update
+echo "${DEFAULT}INFO: Starting Downloading...${ENDCOLOR}"
+apt -qqy install apache2 mariadb-server php php-xml php-mysqli php-curl unzip wget
+echo ""
+echo -e "${GREEN}SUCCESS: Finished Downloading, setting up...${ENDCOLOR}"
+echo ""
+echo -e "${DEFAULT}INFO: Setting Up MySQL Database...\n Please Enter your Options!${ENDCOLOR}"
+mysql_secure_installation
+echo -e "${GREEN}SUCCESS: Finished Setting Up MySQL Database...${ENDCOLOR}"
+echo ""
+echo -e "${DEFAULT}INFO: Setting up PhpMyAdmin...${ENDCOLOR}"
+cd /usr/share
+wget https://files.phpmyadmin.net/phpMyAdmin/5.1.1/phpMyAdmin-5.1.1-all-languages.zip
+unzip phpMyAdmin-5.1.1-all-languages.zip
+rm phpMyAdmin-5.1.1-all-languages.zip
+mv phpMyAdmin-*-all-languages phpmyadmin
+chmod -R 0755 phpmyadmin
+cd /etc/apache2/conf-available
+wget https://pastebin.com/raw/VFAuWSyq && mv VFAuWSyq phpmyadmin.conf
+a2enconf phpmyadmin
+systemctl reload apache2
+mkdir /usr/share/phpmyadmin/tmp
+chown -R www-data:www-data /usr/share/phpmyadmin/tmp/
+mysql --user="root" --execute="UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root' AND plugin = 'unix_socket';"
+echo ""
+echo -e "${GREEN}SUCCESS: Finished Setting Up Everything${ENDCOLOR}"
+echo ""
+adapter=$(ip link | awk -F: '$0 !~ "lo|vir|wl|docker|^[^0-9]"{print $2;getline}')
+ip4=$(/sbin/ip -o -4 addr list $adapter | awk '{print $4}' | cut -d/ -f1)
+echo -e "${DEFAULT}Website is accessable at: http://$ip4/${ENDCOLOR}"
+echo -e "${DEFAULT}PhpMyAdmin is accessable at: http://$ip4/phpmyadmin${ENDCOLOR}"
